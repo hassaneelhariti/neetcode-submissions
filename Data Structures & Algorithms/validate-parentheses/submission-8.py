35e22135e715175291a1330842353121a5534b30
HASSANE ELHARITI <hassaneelhariti@gmail.com>
2026-07-22 15:50:22 +0100

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)<=1:
+            return False
+        dic={"(":")","{":"}","[":"]"}
+        stk=[]
+        for i in s:
+            if i in dic.keys():
+                stk.append(i)
+                continue
+            if stk:
+                if i!=dic[stk.pop()]:
+                    return False
+            else:
+                return False
+        return len(stk) == 0
